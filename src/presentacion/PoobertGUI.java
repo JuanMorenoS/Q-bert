2e2a30b2551187f8555c2b2a14a4d225fd6b92ef
Juan Moreno <juan.andres.208@hotmail.com>
2017-04-10 01:46:53 -0500

    level reader ok

--- src/presentacion/PoobertGUI.java
+++ src/presentacion/PoobertGUI.java
@@ -3,18 +3,23 @@
  */
 package presentacion;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.*;
 import javax.swing.*;
 
 /**
  * @author blackphantom
  *
  */
-public class PoobertGUI extends JFrame{
-	
-	public PoobertGUI(){
+public class PoobertGUI extends JFrame {
+	private UltraPanel tablero;
+
+	public PoobertGUI() {
 		super();
+		setLayout(new BorderLayout());
 		prepareElementos();
 		prepareAcciones();
 	}
@@ -22,13 +27,38 @@ public class PoobertGUI extends JFrame{
 	private void prepareAcciones() {
 		setTitle("Poo*Bert");
 		setSize(600, 600);
+		centre();
 	}
 
 	private void prepareElementos() {
-		UltraPanel u = new UltraPanel(this,"Red","Yellow");
-		add(u);
-		u.changeColor(Color.yellow);
-		
+		try {
+			leerNivel();
+		} catch (IOException e) {
+		}
+		add(tablero, BorderLayout.CENTER);
 	}
-	
+
+	private void leerNivel() throws IOException {
+		BufferedReader in;
+		in = new BufferedReader(new FileReader("resources/Levels/1.level"));
+		tablero = new UltraPanel(this, in.readLine(),in.readLine(), 10);
+		int a=Integer.parseInt(in.readLine());
+		for(int i=0;i<a;i++){
+			int q=0;
+			for(char j:in.readLine().toCharArray()){
+				if(j!='X' && j!='x')
+					tablero.add(i,q);
+				q++;
+			}
+		}
+		in.close();
+	}
+
+	private void centre() {
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		int xEsquina = (screen.width - getSize().width) / 2;
+		int yEsquina = (screen.height - getSize().height) / 2;
+		setLocation(xEsquina, yEsquina);
+	}
+
 }
--- src/presentacion/UltraPanel.java
+++ src/presentacion/UltraPanel.java
@@ -45,20 +45,23 @@ public class UltraPanel extends JPanel {
 			else
 				land.add(new Cube[(600 / (tam * 4)) - 1]);
 		}
+		
 		for (int i = 0; i < 600 / (tam * 4); i++) {
 			for (int j = 0; j < land.get(i).length; j++) {
 				if (i % 2 == 0) {
-					land.get(i)[j] = (new Cube(tam, color));
+					land.get(i)[j] = (new Cube(tam, null));
 					land.get(i)[j].move(j * tam * 4, i * tam * 3);
 				} else {
-					land.get(i)[j] = (new Cube(tam, color));
+					land.get(i)[j] = (new Cube(tam, null));
 					land.get(i)[j].move((j * tam * 4) + tam * 2, i * tam * 3);
 				}
 			}
 		}
 
 	}
-
+	public void add(int x, int y){
+		land.get(x)[y].setColors(color);
+	}
 	public void changeColor(int x,int y) {
 		land.get(x)[y].visited();
 	}
@@ -70,16 +73,10 @@ class Cube {
 
 	public Cube(int tam, String[] colors) {
 		int[] numbers = new int[5];
-		this.colors = new Color[5];
 		numbers[0] = 0;
+		setColors(colors);
 		for (int i = 1; i < 5; i++)
 			numbers[i] = tam * i;
-		if (colors != null) {
-			for (int i = 0; i < 4; i++) {
-				this.colors[i]=stringToColor(colors[i]);
-			}
-		} else
-			this.colors = new Color[] { Color.BLACK, Color.BLACK, Color.black, Color.BLACK };
 		edges = new Polygon[3];
 		edges[0] = new Polygon(new int[] { numbers[0], numbers[2], numbers[4], numbers[2] },
 				new int[] { numbers[1], numbers[2], numbers[1], numbers[0] }, 4);
@@ -110,6 +107,15 @@ class Cube {
 		for (Polygon pol : edges) {
 			pol.translate(x, y);
 		}
+	}
+	public void setColors(String [] colors){
+		this.colors = new Color[5];
+		if (colors != null) {
+			for (int i = 0; i < 4; i++) {
+				this.colors[i]=stringToColor(colors[i]);
+			}
+		} else
+			this.colors = new Color[] { Color.BLACK, Color.BLACK, Color.black, Color.BLACK };
 	}
 
 }
