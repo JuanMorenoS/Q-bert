78b68c123a32667ed2449b39fb471f14ba69a59d
Juan Moreno <Juan.andres.208@hotmail.com>
2017-04-20 12:07:59 -0500

    test

---
+++ src/test/CubeTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CubeTest {
+
+	@Test
+	public void testMove() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetCords() {
+		assertTrue(true);
+	}
+
+}
