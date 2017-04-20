37e864489f3cdf34d485711b6b232eeb4fc4b1ed
Juan Moreno <juan.andres.208@hotmail.com>
2017-04-20 13:07:48 -0500

    start model

--- src/aplicacion/Poobert.java
+++ src/aplicacion/Poobert.java
@@ -1,14 +1,5 @@
 package aplicacion;
 
-import java.io.*;
-
 public class Poobert{
-	private  static BufferedReader in;
-	public static void main(String[] args){
-		try{
-		in = new BufferedReader(new FileReader("<resources/Levels/1.level>"));}
-		catch (Exception e) {
-		}
-		
-	}
+	
 }
