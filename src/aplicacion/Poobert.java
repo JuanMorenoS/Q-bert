c7fbe992b11507f78cd0c8724b1402c4c2cd40a3
Juan Moreno <juan.andres.208@hotmail.com>
2017-04-21 00:43:34 -0500

    add arts,refactoring and star model

--- src/aplicacion/Poobert.java
+++ src/aplicacion/Poobert.java
@@ -1,5 +1,117 @@
 package aplicacion;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import presentacion.Drawer;
+
 public class Poobert{
+	private ArrayList<Cube[]> land;
+	private String[] color;
+	private int tam;
+	private Player[] players;
+	private int level,xLevel,yLevel;
+	private String[] playersNames;
+	private char selection;
 	
+	public Poobert(String[] strings, char selection) {
+		level=1;
+		players=new Player[2];
+		playersNames=strings;
+		this.selection=selection;
+		try {
+			leerNivel(level);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void tablero(int b) {
+		land = new ArrayList<>();
+		for (int i = 0; i < b; i++) {
+			if (i % 2 == 0)
+				land.add(new Cube[b]);
+			else
+				land.add(new Cube[(b)]);
+		}
+		for (int i = 0; i < b; i++) {
+			for (int j = 0; j < land.get(i).length; j++) {
+				if (i % 2 == 0) {
+					land.get(i)[j] = (new Cube(tam, null));
+					land.get(i)[j].move(j * tam * 4, i * tam * 3);
+				} else {
+					land.get(i)[j] = (new Cube(tam, null));
+					land.get(i)[j].move((j * tam * 4) + tam * 2, i * tam * 3);
+				}
+			}
+		}
+	}
+	public void add(int x, int y) {
+		land.get(x)[y].setColors(color,false);
+	}
+
+	public void changeColor(Player play) {
+		if (!land.get(play.cy)[play.cx].visited())
+			play.lose('F');
+		
+	}
+	public void setPlayer1(int i, int q,String name) {
+		int [] temo = land.get(i)[q].getCords();
+		players[0] = new Player(temo[0], temo[1],tam,q,i,name,'n');
+	}
+	public void setPlayer2(int i, int q,String name) {
+		int [] temo = land.get(i)[q].getCords();
+		players[1] = new Player(temo[0], temo[1],tam,q,i,name,'b');
+	}
+	
+	private void leerNivel(int le) throws IOException {
+		int zoom=20 *3;
+		tam=zoom/3;
+		BufferedReader in;
+		in = new BufferedReader(new FileReader("resources/Levels/"+le+".level"));
+		xLevel=Integer.parseInt(in.readLine());
+		yLevel=Integer.parseInt(in.readLine());
+		color = new String[] { in.readLine(), "lightGray", "darkGray", in.readLine() };
+		tablero(yLevel);
+		for(int i=0;i<xLevel;i++){
+			int q=0;
+			for(char j:in.readLine().toCharArray()){ 
+				if(j!='x')
+					add(i,q);
+					if(j=='Q')
+						setPlayer1(i,q,playersNames[0]);
+					if(j=='P' && selection!='1')
+						setPlayer2(i,q,playersNames[1]);
+				if (j!='*')
+					q++;
+			}
+		}
+		in.close();
+	}
+
+	public void move1(String a, String b) {
+		players[0].move(a, b);
+		changeColor(players[0]);
+	}
+	public void move2(String a, String b) {
+		players[1].move(a, b);
+		changeColor(players[1]);
+	}
+
+	public Player[] getPlayers() {
+		return players;
+	}
+
+	public ArrayList<Cube[]> getLand() {
+		return land;
+	}
+
+	public int getSizeX() {
+		return xLevel*20*3;
+	}
+	public int getSizeY() {
+		return yLevel*20*3;
+	}
 }
--- src/presentacion/Drawer.java
+++ src/presentacion/Drawer.java
@@ -20,23 +20,24 @@ import javax.swing.Timer;
 
 import aplicacion.Cube;
 import aplicacion.Player;
+import aplicacion.Poobert;
 
 
 /**
  * @author blackphantom
  *
  */
-public class UltraPanel extends JPanel implements ActionListener,KeyListener{
+public class Drawer extends JPanel implements ActionListener,KeyListener{
 	private Timer time = new Timer(5, this);
-	private ArrayList<Cube[]> land;
-	private String[] color;
-	private int tam;
-	private Player[] players;
 	private PoobertGUI father;
+	private Poobert logical;
+	
+	
+
 	public void paintComponent(Graphics g) {
 		setBackground(Color.black);
 		super.paintComponent(g);
-		for (Cube[] cs : land) {
+		for (Cube[] cs : logical.getLand()) {
 			for (Cube u : cs) {
 				for (int i = 0; i < 3; i++) {
 					g.setColor(u.colors[i]);
@@ -44,51 +45,21 @@ public class UltraPanel extends JPanel implements ActionListener,KeyListener{
 				}
 			}
 		}
-		for (Player qbert:players) if(qbert!=null) g.drawImage(qbert.getDraw(), qbert.x, qbert.y, null);		
+		for (Player qbert:logical.getPlayers()) if(qbert!=null) g.drawImage(qbert.getDraw(), qbert.x, qbert.y, null);		
 	}
 
-	public UltraPanel(int b,PoobertGUI god, String in, String fi, int x) {
+	public Drawer(PoobertGUI god) {
 		time.start();
 		father=god;
-		players=new Player[2];
+		logical=new Poobert(new String[]{father.getPlayer1Name(),father.getPlayer2Name()},father.getSelection());
+		father.setSize(logical.getSizeY(), logical.getSizeX());
 		addKeyListener(this);
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);
-		tam = x;
-		color = new String[] { in, "lightGray", "darkGray", fi };
-		tablero(b);
-	}
-
-	public void add(int x, int y) {
-		land.get(x)[y].setColors(color,false);
-	}
-
-	public void changeColor(int x, int y) {
-		if (!land.get(x)[y].visited())
-			players[0].lose('F');
 		
 	}
 
-	private void tablero(int b) {
-		land = new ArrayList<>();
-		for (int i = 0; i < b; i++) {
-			if (i % 2 == 0)
-				land.add(new Cube[b]);
-			else
-				land.add(new Cube[(b)]);
-		}
-		for (int i = 0; i < b; i++) {
-			for (int j = 0; j < land.get(i).length; j++) {
-				if (i % 2 == 0) {
-					land.get(i)[j] = (new Cube(tam, null));
-					land.get(i)[j].move(j * tam * 4, i * tam * 3);
-				} else {
-					land.get(i)[j] = (new Cube(tam, null));
-					land.get(i)[j].move((j * tam * 4) + tam * 2, i * tam * 3);
-				}
-			}
-		}
-	}
+	
 
 	public void actionPerformed(ActionEvent e) {
 		repaint();		
@@ -104,46 +75,31 @@ public class UltraPanel extends JPanel implements ActionListener,KeyListener{
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
 		if(key == 103){
-			players[0].move("L","U");
-			changeColor(players[0].cy, players[0].cx);
+			logical.move1("L","U");
 		}
 		if(key == 105){
-			players[0].move("R","U");
-			changeColor(players[0].cy, players[0].cx);
+			logical.move1("R","U");
 		}
 		if(key == 97){
-			players[0].move("L","D");
-			changeColor(players[0].cy, players[0].cx);
+			logical.move1("L","D");
 		}
 		if(key == 99){
-			players[0].move("R","D");
-			changeColor(players[0].cy, players[0].cx);
+			logical.move1("R","D");
 		}
 		if(key == 81 && father.getSelection()!='C'){
-			players[1].move("L","U");
-			changeColor(players[1].cy, players[1].cx);
+			logical.move2("L","U");
 		}
 		if(key == 87 && father.getSelection()!='C'){
-			players[1].move("R","U");
-			changeColor(players[1].cy, players[1].cx);
+			logical.move2("R","U");
 		}
 		if(key == 65 && father.getSelection()!='C'){
-			players[1].move("L","D");
-			changeColor(players[1].cy, players[1].cx);
+			logical.move2("L","D");
 		}
 		if(key == 83 && father.getSelection()!='C'){
-			players[1].move("R","D");
-			changeColor(players[1].cy, players[1].cx);
+			logical.move2("R","D");
 		}
 	}
 
-	public void setPlayer1(int i, int q,String name) {
-		int [] temo = land.get(i)[q].getCords();
-		players[0] = new Player(temo[0], temo[1],tam,q,i,name,'n');
-	}
-	public void setPlayer2(int i, int q,String name) {
-		int [] temo = land.get(i)[q].getCords();
-		players[1] = new Player(temo[0], temo[1],tam,q,i,name,'b');
-	}
+	
 }
 
