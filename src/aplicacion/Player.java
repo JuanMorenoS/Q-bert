d5263c479a50ce605678f498d6587a9b0544b857
Blackphantom96 <juan.andres.208@hotmail.com>
2017-04-26 14:23:06 -0500

    Update Player.java

--- src/aplicacion/Player.java
+++ src/aplicacion/Player.java
@@ -21,8 +21,9 @@ public class Player {
 	private String name;
 	private int lives;
 	private char color;
-	public Player(int posx, int posy, int tam, int cx, int cy, String name,char col) {
-		color=col;
+
+	public Player(int posx, int posy, int tam, int cx, int cy, String name, char col) {
+		color = col;
 		origen = new int[] { posx - (size * 3) / 2, posy - size * 3, cx, cy };
 		this.name = name;
 		lives = 3;
@@ -42,7 +43,7 @@ public class Player {
 			this.representation = null;
 			size = 0;
 		}
-		refresImage("D","R");
+		refresImage("D", "R");
 	}
 
 	private void resetPosition() {
@@ -52,33 +53,37 @@ public class Player {
 		cy = origen[3];
 	}
 
-	public void move(String a, String b) {
-		int velx = size * 2;
-		int vely = size * 3;
-		if (a.equals("L"))
-			velx *= -1;
-		if (b.equals("U"))
-			vely *= -1;
-		x += velx;
-		y += vely;
-		if (size != 0) {
-			if (cy % 2 != 0) {
-				cx = (a.equals("L")) ? cx : cx + 1;
-			} else {
-				cx = (a.equals("R")) ? cx : cx - 1;
+	public boolean move(String a, String b) {
+		if (lives > 0) {
+			int velx = size * 2;
+			int vely = size * 3;
+			if (a.equals("L"))
+				velx *= -1;
+			if (b.equals("U"))
+				vely *= -1;
+			x += velx;
+			y += vely;
+			if (size != 0) {
+				if (cy % 2 != 0) {
+					cx = (a.equals("L")) ? cx : cx + 1;
+				} else {
+					cx = (a.equals("R")) ? cx : cx - 1;
+				}
+				cy = (b.equals("U")) ? cy - 1 : cy + 1;
 			}
-			cy = (b.equals("U")) ? cy - 1 : cy + 1;
+			refresImage(b, a);
+			return true;
 		}
-		refresImage(b,a);
+		return false;
 	}
 
 	public Image getDraw() {
 		return representation;
 	}
 
-	private void refresImage(String b,String a) {
-		image = new ImageIcon("resources/qbert"+color+"-"+b+a+".png");
-		this.representation = image.getImage().getScaledInstance(size * 3, size * 3, Image.SCALE_DEFAULT);		
+	private void refresImage(String b, String a) {
+		image = new ImageIcon("resources/qbert" + color + "-" + b + a + ".png");
+		this.representation = image.getImage().getScaledInstance(size * 3, size * 3, Image.SCALE_DEFAULT);
 	}
 
 }
