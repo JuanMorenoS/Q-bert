079818b0615abf5d1b25d25fe0304489358896a4
Juan Moreno <juan.andres.208@hotmail.com>
2017-04-26 23:02:59 -0500

    fix resize level

--- src/aplicacion/Poobert.java
+++ src/aplicacion/Poobert.java
@@ -5,22 +5,20 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import presentacion.Drawer;
-
-public class Poobert{
+public class Poobert {
 	private ArrayList<Cube[]> land;
 	private String[] color;
 	private int tam;
 	private Player[] players;
-	private int level,xLevel,yLevel;
+	private int level, xLevel, yLevel;
 	private String[] playersNames;
 	private char selection;
-	
+
 	public Poobert(String[] strings, char selection) {
-		level=1;
-		players=new Player[2];
-		playersNames=strings;
-		this.selection=selection;
+		level = 1;
+		players = new Player[2];
+		playersNames = strings;
+		this.selection = selection;
 		try {
 			leerNivel(level);
 		} catch (IOException e) {
@@ -48,43 +46,46 @@ public class Poobert{
 			}
 		}
 	}
+
 	public void add(int x, int y) {
-		land.get(x)[y].setColors(color,false);
+		land.get(x)[y].setColors(color, false);
 	}
 
 	public void changeColor(Player play) {
 		if (!land.get(play.cy)[play.cx].visited())
 			play.lose('F');
-		
+
 	}
-	public void setPlayer1(int i, int q,String name) {
-		int [] temo = land.get(i)[q].getCords();
-		players[0] = new Player(temo[0], temo[1],tam,q,i,name,'n');
+
+	public void setPlayer1(int i, int q, String name) {
+		int[] temo = land.get(i)[q].getCords();
+		players[0] = new Player(temo[0], temo[1], tam, q, i, name, 'n');
 	}
-	public void setPlayer2(int i, int q,String name) {
-		int [] temo = land.get(i)[q].getCords();
-		players[1] = new Player(temo[0], temo[1],tam,q,i,name,'b');
+
+	public void setPlayer2(int i, int q, String name) {
+		int[] temo = land.get(i)[q].getCords();
+		players[1] = new Player(temo[0], temo[1], tam, q, i, name, 'b');
 	}
-	
+
 	private void leerNivel(int le) throws IOException {
-		int zoom=20 *3;
-		tam=zoom/3;
+		int zoom = 10 * 3;
+		tam = zoom / 3;
 		BufferedReader in;
-		in = new BufferedReader(new FileReader("resources/Levels/"+le+".level"));
-		xLevel=Integer.parseInt(in.readLine());
-		yLevel=Integer.parseInt(in.readLine());
+		in = new BufferedReader(new FileReader("resources/Levels/" + le + ".level"));
+		xLevel = Integer.parseInt(in.readLine());
+		yLevel = Integer.parseInt(in.readLine());
 		color = new String[] { in.readLine(), "lightGray", "darkGray", in.readLine() };
 		tablero(yLevel);
-		for(int i=0;i<xLevel;i++){
-			int q=0;
-			for(char j:in.readLine().toCharArray()){ 
-				if(j!='x')
-					add(i,q);
-					if(j=='Q')
-						setPlayer1(i,q,playersNames[0]);
-					if(j=='P' && selection!='1')
-						setPlayer2(i,q,playersNames[1]);
-				if (j!='*')
+		for (int i = 0; i < xLevel; i++) {
+			int q = 0;
+			for (char j : in.readLine().toCharArray()) {
+				if (j != 'x')
+					add(i, q);
+				if (j == 'Q')
+					setPlayer1(i, q, playersNames[0]);
+				if (j == 'P' && selection != '1')
+					setPlayer2(i, q, playersNames[1]);
+				if (j != '*')
 					q++;
 			}
 		}
@@ -92,12 +93,13 @@ public class Poobert{
 	}
 
 	public void move1(String a, String b) {
-		players[0].move(a, b);
-		changeColor(players[0]);
+		if (players[0].move(a, b))
+			changeColor(players[0]);
 	}
+
 	public void move2(String a, String b) {
-		players[1].move(a, b);
-		changeColor(players[1]);
+		if (players[1].move(a, b))
+			changeColor(players[1]);
 	}
 
 	public Player[] getPlayers() {
@@ -109,9 +111,10 @@ public class Poobert{
 	}
 
 	public int getSizeX() {
-		return xLevel*20*3;
+		return xLevel*tam*2+tam*10;
 	}
+
 	public int getSizeY() {
-		return yLevel*20*3;
+		return yLevel*tam*4+tam*2;
 	}
 }
